5e5a54811dd2b0166a33eaa7e683f3fa7931aee1
kaiiquecosta <contatokaiiquecosta@gmail.com>
2025-02-10 20:53:59 -0300

    Nova split e submenu

--- FontesVCL/UnitPrincipal.pas
+++ FontesVCL/UnitPrincipal.pas
@@ -6,7 +6,7 @@ uses
   Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
   Vcl.Controls, Vcl.Forms, Vcl.Dialogs, System.ImageList, Vcl.ImgList,
   Vcl.Imaging.pngimage, Vcl.ExtCtrls, Vcl.Buttons, Vcl.WinXCtrls,
-  Vcl.CategoryButtons;
+  Vcl.CategoryButtons, Vcl.StdCtrls;
 
 type
   TfrmPrincipal = class(TForm)
@@ -16,9 +16,17 @@ type
     Image1: TImage;
     imageList: TImageList;
     CategoryMenuButtons: TCategoryButtons;
+    sSubMenu: TSplitView;
+    Panel1: TPanel;
+    CategorySubMenuButtons: TCategoryButtons;
+    Label1: TLabel;
+    btnCloseSub: TSpeedButton;
     procedure btnMenuClick(Sender: TObject);
+    procedure btnCloseSubClick(Sender: TObject);
+    procedure CategoryMenuButtonsCategories0Items2Click(Sender: TObject);
   private
     { Private declarations }
+  procedure CloseSubMenu(Sender: TObject);
   public
     { Public declarations }
   end;
@@ -30,9 +38,25 @@ implementation
 
 {$R *.dfm}
 
+procedure TfrmPrincipal.btnCloseSubClick(Sender: TObject);
+begin
+  CloseSubMenu;
+end;
+
 procedure TfrmPrincipal.btnMenuClick(Sender: TObject);
 begin
   sMenu.Opened := not sMenu.Opened;
+end;
+
+procedure CloseSubMenu;
+begin
+  sSubMenu.Opened := not sSubMenu.Opened
+end;
+
+procedure TfrmPrincipal.CategoryMenuButtonsCategories0Items2Click(
+  Sender: TObject);
+begin
+  sSubMenu.Opened := true;
 end;
 
 end.
